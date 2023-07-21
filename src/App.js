753418dcb1624118c42a6150332442a0c5acd0ae
o-RoGerSLegacy-o <shubhamhugay@gmail.com>
2023-07-21 13:47:48 +0530

    useStatePractised

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import BookList from "./components/BookList";
+import StateExamples from "./statepractise/StateExamples";
 
 function App() {
   return (
     <>
       <BookList />
+      <StateExamples />
     </>
   );
 }
